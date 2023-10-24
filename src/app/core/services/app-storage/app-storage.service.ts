64e293c8d6187d3b90bfe244b1e99a28c8641666
Jakub Szendzielorz <kubasz15@gmail.com>
2023-10-24 21:11:07 +0200

    fixed some bugs and working on update method

--- src/app/core/services/app-storage/app-storage.service.ts
+++ src/app/core/services/app-storage/app-storage.service.ts
@@ -6,33 +6,40 @@ import {Storage} from '@ionic/storage-angular';
 })
 export class AppStorageService {
   private _storage: Storage | null = null;
+  private _id: number = 0;
 
   constructor(private storage: Storage) { }
-
   async init() {
     this._storage = await this.storage.create();
+    if(await this._storage?.get('id') == null) {
+        await this._storage.set('id', this._id).then(value => value);
+    }
+  }
+
+  public async incrementID(): Promise<number> {
+      let id: number = await this._storage?.get('id').then(value => value)
+      id++;
+      return this._storage?.set('id', id).then(value => value);
   }
 
   public async set(key: string, value: any): Promise<void> {
-    await this._storage?.set(key, value).then(value => { return value });
+    await this._storage?.set(key, value).then(value => value);
   }
 
   public async get(key: string): Promise<any> {
-    return this._storage?.get(key).then(value => {
-      return value || null
-    });
+    return this._storage?.get(key).then(value => value || null );
   }
 
   public async remove(key: string): Promise<void> {
-    await this._storage?.remove(key).then(value => { return value });
+    await this._storage?.remove(key).then(value => value);
   }
 
   public async clear(): Promise<void> {
-    await this._storage?.clear().then(value => { return value });
+    await this._storage?.clear().then(value => value);
   }
 
   public async keys(): Promise<void> {
-    await this._storage?.keys().then(value => { return value });
+    await this._storage?.keys().then(value => value);
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {DialogService} from '../core';
-import {UntypedFormControl, UntypedFormGroup} from '@angular/forms';
-import {Barcode, BarcodeFormat, BarcodeScanner, BarcodeValueType, LensFacing} from '@capacitor-mlkit/barcode-scanning';
-import {FilePicker} from '@capawesome/capacitor-file-picker';
-import {BarcodeScanningModalComponent} from '../barcode-scanning-modal/barcode-scanning-modal.component';
-import {AppStorageService} from '../core/services/app-storage/app-storage.service';
-import {Card} from "../core/interface/card";
-import {AddCardsFormComponent} from "../add-cards-form/add-cards-form.component";
+import { Component, OnInit } from '@angular/core';
+import { DialogService } from '../core';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { Barcode, BarcodeFormat, BarcodeScanner, BarcodeValueType, LensFacing } from '@capacitor-mlkit/barcode-scanning';
+import { FilePicker } from '@capawesome/capacitor-file-picker';
+import { BarcodeScanningModalComponent } from '../barcode-scanning-modal/barcode-scanning-modal.component';
+import { AppStorageService } from '../core/services/app-storage/app-storage.service';
+import { Card } from "../core/interface/card";
+import { AddCardsFormComponent } from "../add-cards-form/add-cards-form.component";
 
 @Component({
   selector: 'app-Home',
@@ -14,16 +14,14 @@ import {AddCardsFormComponent} from "../add-cards-form/add-cards-form.component"
   styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit {
-  public value: any = '';
-
   public map: Map<string, any> = new Map();
 
-  public test: Card[] = [];
+  public cards: Card[] = [];
+  public _id: number = 0;
 
   public readonly barcodeFormat = BarcodeFormat;
   public readonly lensFacing = LensFacing;
   public barcodes: Barcode[] = [];
-  public cards: Card[] = [];
   public isSupported = false;
   public isPermissionGranted = false;
 
@@ -40,128 +38,76 @@ export class HomePage implements OnInit {
   }
 
   async ngOnInit() {
-    // await this.setValue();
-    // const map = await this.appStorageService.get('my-map');
-    // const cards: Card[] = [];
-    //
-    // for(const card of map.values()) {
-    //   cards.push(card);
-    // }
-    //
-    // this.cards = cards;
-
-    this.cards = await this.appStorageService.get('my-cards');
-
-    BarcodeScanner.isSupported().then((result) => {
-      this.isSupported = result.supported;
-    });
-    BarcodeScanner.checkPermissions().then((result) => {
-      this.isPermissionGranted = result.camera === 'granted';
-    });
+    if (await this.appStorageService.get('my-cards') != null) {
+      this.cards = await this.appStorageService.get('my-cards');
+    }
+    // BarcodeScanner.isSupported().then((result) => {
+    //   this.isSupported = result.supported;
+    // });
+    // BarcodeScanner.checkPermissions().then((result) => {
+    //   this.isPermissionGranted = result.camera === 'granted';
+    // });
   }
 
 
-  async remove(no: any) {
+  async remove(i: any) {
     const arrayOfCards = await this.appStorageService.get('my-cards');
-    const val: Card = arrayOfCards.find((v: { id: number; }) => v.id === 1)
-    const newArrayOfCards = arrayOfCards.filter((e: any) => e != val);
-    console.log(newArrayOfCards);
-    await this.appStorageService.set('my-cards', newArrayOfCards);
+    const val: Card = arrayOfCards.find((v: { id: number; }) => v.id === i)
+    this.cards = arrayOfCards.filter((e: any) => e != val);
+    await this.appStorageService.set('my-cards', this.cards);
   };
 
 
   async setValue() {
     this.cards.push({
+      id: this._id,
       shopName: `dasdasdasd ${Date.now()}`,
       shopLocalization: 'pcim',
       modified: new Date().toLocaleString("pl-PL"),
-      id: 1,
-      barcode: {
-        rawValue: '',
-        valueType: BarcodeValueType.Unknown,
-        displayValue: 'test',
-        format: BarcodeFormat.Aztec,
-        bytes:  [33, 263],
-      }
-    })
-    this.cards.push({
-      shopName: `dasdasdasd ${Date.now()}`,
-      shopLocalization: 'pcim',
-      modified: new Date().toLocaleString("pl-PL"),
-      id: 2,
-      barcode: {
-        rawValue: '',
-        valueType: BarcodeValueType.Unknown,
-        displayValue: 'ewa',
-        format: BarcodeFormat.Aztec,
-        bytes:  [33, 263],
-      }
-    })
-    this.cards.push({
-      shopName: `dasdasdasd ${Date.now()}`,
-      shopLocalization: 'pcim',
-      modified: new Date().toLocaleString("pl-PL"),
-      id: 3,
       barcode: {
         rawValue: '',
         valueType: BarcodeValueType.Unknown,
         displayValue: 'adam',
         format: BarcodeFormat.Aztec,
-        bytes:  [33, 263],
+        bytes: [33, 263],
       }
     })
-    // console.log(this.test)
 
-    const val = this.cards.find(v => v.id === 1)
-    if (val) {
-      console.log(val.shopName); // "Alice"
-    } else {
-      console.log("No shop found with ID 1");
-    }
-
+    this._id++;
     await this.appStorageService.set('my-cards', this.cards);
+    await this.appStorageService.set('id', this._id);
     const objReturn = await this.appStorageService.get('my-cards');
-
     console.log(objReturn);
 
   }
 
   async getValue() {
-    const arrayOfCards = await  this.appStorageService.get('my-cards');
+    const arrayOfCards = await this.appStorageService.get('my-cards');
     const val: Card = arrayOfCards.find((v: { id: number; }) => v.id === 1)
     if (val) {
       console.log(val.shopName); // "Alice"
     } else {
       console.log("No shop found with ID 1");
-    }  }
-
-  async updateCard(key: string, card: Card) {
-    // Get the map from local storage.
-    const map = await this.appStorageService.get('my-map');
-
-    // Get the card from the map.
-    const cardToUpdate: Card = map.get(key);
-
-    // Update the values of the card.
-    cardToUpdate.shopName = card.shopName;
-    cardToUpdate.barcode = card.barcode;
-    cardToUpdate.shopLocalization = card.shopLocalization;
-    cardToUpdate.modified = card.modified;
-
-    map.set(key, cardToUpdate);
-
-    await this.appStorageService.set('my-map', map);
+    }
   }
 
-  async removeValue() {
-    const arrayOfCards = await this.appStorageService.get('my-cards');
-    const val: Card = arrayOfCards.find((v: { id: number; }) => v.id === 1)
-    const newArrayOfCards = arrayOfCards.filter((e: any) => e != val);
-    console.log(newArrayOfCards);
-    await this.appStorageService.set('my-cards', newArrayOfCards);
-  }
-
-
+  // async updateCard(key: string, card: Card) {
+  //   // Get the map from local storage.
+  //   const map = await this.appStorageService.get('my-map');
+  //
+  //   // Get the card from the map.
+  //   const cardToUpdate: Card = map.get(key);
+  //
+  //   // Update the values of the card.
+  //   cardToUpdate.shopName = card.shopName;
+  //   cardToUpdate.barcode = card.barcode;
+  //   cardToUpdate.shopLocalization = card.shopLocalization;
+  //   cardToUpdate.modified = card.modified;
+  //
+  //   map.set(key, cardToUpdate);
+  //
+  //   await this.appStorageService.set('my-map', map);
+  // }
 
   public async startScan(): Promise<void> {
     const formats = this.formGroup.get('formats')?.value || [];
@@ -185,15 +131,6 @@ export class HomePage implements OnInit {
     await this.showAddCardForm();
   }
 
-  async showAddCardForm() {
-    const formElement = await this.dialogService.showModal({
-      component: AddCardsFormComponent
-    })
-    formElement.onDidDismiss().then((result) => {
-      console.log(result);
-    })
-  }
-
   public async readBarcodeFromImage(): Promise<void> {
     const { files } = await FilePicker.pickImages({ multiple: false });
     const path = files[0]?.path;
@@ -209,6 +146,13 @@ export class HomePage implements OnInit {
     await this.showAddCardForm();
   }
 
-
+  async showAddCardForm() {
+    const formElement = await this.dialogService.showModal({
+      component: AddCardsFormComponent
+    })
+    formElement.onDidDismiss().then((result) => {
+      console.log(result);
+    })
+  }
 
 }
